ce942573bda8b1167fda2278ac5a9b4b3c66c520
sdeglurkar <sampada@visual-pomdp-test-1.c.uncertainty-aware-learning.internal>
2022-04-05 02:19:43 +0000

    Applied the bug fix to the Stanford env too

--- control/stanford.py
+++ control/stanford.py
@@ -9,9 +9,7 @@ import gym
 from collections import OrderedDict
 from planet.control.abstract import AbstractEnvironment
 from planet.humanav_examples.examples import *
-#from planet.humanav.humanav_renderer import HumANavRenderer
-#from planet.humanav.renderer_params import create_params as create_base_params
-#from planet.humanav.renderer_params import get_surreal_texture_dir
+
 
 def check_path(path):
     if not os.path.exists(path):
@@ -49,10 +47,12 @@ class Stanford_Environment_Params():
 
 sep = Stanford_Environment_Params()
 
+
 class StanfordEnvironment(AbstractEnvironment):
     def __init__(self, disc_thetas=False):
-        self.done = False
+        #self.done = False
         self._step = None
+        self.reached_goal = False
         self.true_env_corner = [24.0, 23.0] 
         self.xrange = [0, 8.5]
         self.yrange = [0, 1.5]
@@ -98,10 +98,62 @@ class StanfordEnvironment(AbstractEnvironment):
         self.testing_data_files = glob.glob(self.testing_data_path)
         self.normalization = sep.normalization
 
+    def initial_state(self):
+        if self.disc_thetas:
+            orientation = self.discrete_thetas[np.random.randint(len(self.discrete_thetas))][0]
+        else:
+            orientation = np.random.rand()  
+            orientation = orientation * (self.thetas[1] - self.thetas[0]) + self.thetas[0]
+ 
+        valid_state = False
+        while not valid_state:
+            state = np.random.rand(sep.dim_state)
+            temp = state[1]
+            state[0] = state[0] * (self.init_strip_x[1] - self.init_strip_x[0]) + self.init_strip_x[0]
+            state[1] = temp * (self.trap_y[1] - self.trap_y[0]) + self.trap_y[0]  # Only consider x for in_trap
+            if not self.in_trap(state) and not self.in_goal(state):
+                valid_state = True
+                state[1] = temp * (self.init_strip_y[1] - self.init_strip_y[0]) + self.init_strip_y[0]
 
-    @property
-    def action_space(self):
-        return gym.spaces.Box(low=np.array([-1.0]), high=np.array([1.0]), dtype=np.float32)
+        return state, orientation
+
+    def in_trap(self, state):
+        # Returns true if in trap
+        first_trap = self.trap_x[0]
+        first_trap_x = (state[0] >= first_trap[0] and state[0] <= first_trap[1])
+        second_trap = self.trap_x[1]
+        second_trap_x = (state[0] >= second_trap[0] and state[0] <= second_trap[1])
+        trap_x = first_trap_x or second_trap_x
+
+        trap = trap_x and (state[1] >= self.trap_y[0] and state[1] <= self.trap_y[1]) 
+
+        # Traps that may appear during test time -- optional
+        if self.test_trap:
+            first_test_trap = self.test_trap_x[0]
+            first_test_trap_x = (state[0] >= first_test_trap[0] and state[0] <= first_test_trap[1])
+            second_test_trap = self.test_trap_x[1]
+            second_test_trap_x = (state[0] >= second_test_trap[0] and state[0] <= second_test_trap[1])
+            #test_trap_x = first_test_trap_x or second_test_trap_x
+
+            first_test_trap = self.test_trap_y[0]
+            first_test_trap_y = (state[1] >= first_test_trap[0] and state[1] <= first_test_trap[1])
+            second_test_trap = self.test_trap_y[1]
+            second_test_trap_y = (state[1] >= second_test_trap[0] and state[1] <= second_test_trap[1])
+            
+            test_trap = (first_test_trap_x and first_test_trap_y) or \
+                        (second_test_trap_x and second_test_trap_y)
+
+            # test_trap = test_trap_x and (state[1] >= self.test_trap_y[0] and state[1] <= self.test_trap_y[1])
+            
+            return (trap or test_trap) and not self.in_goal(state)
+        
+        return trap and not self.in_goal(state) 
+
+    def in_goal(self, state):
+        # Returns true if in goal
+        goal = (state[0] >= self.target_x[0] and state[0] <= self.target_x[1]) and \
+                (state[1] >= self.target_y[0] and state[1] <= self.target_y[1])
+        return goal
 
     @property
     def observation_space(self):  ## TODO Include pixel wrapper and don't normalize
@@ -111,6 +163,10 @@ class StanfordEnvironment(AbstractEnvironment):
         high = np.ones([64, 64, 3], dtype=np.float32)
         spaces = {'image': gym.spaces.Box(low, high)}
         return gym.spaces.Dict(spaces)
+
+    @property
+    def action_space(self):
+        return gym.spaces.Box(low=np.array([-1.0]), high=np.array([1.0]), dtype=np.float32)
     
     def set_test_trap(self, test_trap_is_random=False):
         self.test_trap = True
@@ -122,7 +178,8 @@ class StanfordEnvironment(AbstractEnvironment):
     #     return obs
 
     def reset(self):
-        self.done = False
+        #self.done = False
+        self.reached_goal = False
         self._step = 0
         self.state, self.orientation = self.initial_state()
 
@@ -150,25 +207,151 @@ class StanfordEnvironment(AbstractEnvironment):
         #obs = self.observation_space.sample()
 
         return obs
+    
+    # def step(self, action):
+    #     obs = self.observation_space.sample()
+    #     # reward = self._random.uniform(0, 1)
+    #     reward = np.random.uniform(0, 1)
+    #     self._step += 1
+    #     #done = self._step >= 1000
+    #     done = self._step >= 10
+    #     #done = self._step >= 200
+    #     info = {}
+    #     return obs, reward, done, info
 
-    def initial_state(self):
-        if self.disc_thetas:
-            orientation = self.discrete_thetas[np.random.randint(len(self.discrete_thetas))][0]
+    def step(self, action, action_is_vector=False):
+        episode_length = 50
+
+        #self.done = False
+        curr_state = self.state
+
+        # Get the observation at the current state to provide PlaNet the expected output
+        normalization_data = self.preprocess_data()
+        obs_nav, _, _, _ = self.get_observation(normalization_data=normalization_data)
+        obs = OrderedDict()        
+        obs['image'] = obs_nav
+
+        #obs = self.observation_space.sample()
+
+        self._step += 1
+        
+        if action_is_vector:
+            new_theta = np.arctan2(action[1], action[0])
+            if new_theta < 0:  # Arctan stuff
+                new_theta += 2*np.pi
+            next_state = curr_state + action
         else:
-            orientation = np.random.rand()  
-            orientation = orientation * (self.thetas[1] - self.thetas[0]) + self.thetas[0]
- 
-        valid_state = False
-        while not valid_state:
-            state = np.random.rand(sep.dim_state)
-            temp = state[1]
-            state[0] = state[0] * (self.init_strip_x[1] - self.init_strip_x[0]) + self.init_strip_x[0]
-            state[1] = temp * (self.trap_y[1] - self.trap_y[0]) + self.trap_y[0]  # Only consider x for in_trap
-            if not self.in_trap(state) and not self.in_goal(state):
-                valid_state = True
-                state[1] = temp * (self.init_strip_y[1] - self.init_strip_y[0]) + self.init_strip_y[0]
+            new_theta = action[0] * np.pi + np.pi
+            vector = np.array([np.cos(new_theta), np.sin(new_theta)]) * sep.velocity  # Go in the direction the new theta is
+            next_state = curr_state + vector
+    
+        cond_hit = self.detect_collision(next_state)
 
-        return state, orientation 
+        # Previous value of reached_goal 
+        temp_reached_goal = self.reached_goal
+
+        # If already reached goal, don't move
+        if not temp_reached_goal:
+            if self.in_goal(next_state):
+                self.state = next_state
+                self.orientation = new_theta
+                self.reached_goal = True
+                #self.done = True
+            elif cond_hit == False:  # If collided, don't move. Else move.
+                self.state = next_state
+                self.orientation = new_theta
+        #reward = sep.epi_reward * self.done
+
+        # If we just reached the goal, collect reward
+        # But if we already reached the goal previously, get reward 0
+        if not temp_reached_goal and self.reached_goal:
+            reward = sep.epi_reward 
+        else:
+            reward = 0
+        
+        # If already reached goal, don't reason about trap rewards
+        if not temp_reached_goal:
+            cond_false = self.in_trap(next_state)
+            reward -= sep.epi_reward * cond_false
+
+        self.done = self._step >= episode_length
+
+        info = {}
+        return obs, reward, self.done, info
+    
+    def point_to_map(self, pos_2, cast_to_int=True):
+        """
+        Convert pos_2 in real world coordinates
+        to a point on the map.
+        """
+        map_pos_2 = pos_2 / self.dx - self.map_origin
+        if cast_to_int:
+            map_pos_2 = map_pos_2.astype(np.int32)
+        return map_pos_2
+    
+    def detect_collision(self, state):
+        # Returns true if you collided
+        # Map value 0 means there's an obstacle there
+
+        # Don't hit rectangle boundaries
+        if state[0] < self.xrange[0] or state[0] > self.xrange[1]:
+            return True 
+        if state[1] < self.yrange[0] or state[1] > self.yrange[1]:
+            return True
+
+        # Check if state y-value is the same as trap/goal but it's not in the trap or goal - that's a wall
+        # if self.in_trap([self.trap_x[0][0], state[1]]) and \
+        #     not self.in_trap(state) and not self.in_goal(state):
+        #     return True
+        if (state[1] >= self.trap_y[0] and state[1] <= self.trap_y[1]) and \
+            not self.in_trap(state) and not self.in_goal(state):
+            return True
+
+        map_state = self.point_to_map(np.array(state[:sep.dim_state] + self.true_env_corner))
+        map_value = self.traversible[map_state[1], map_state[0]]
+        collided = (map_value == 0)
+        return collided
+    
+    def get_observation(self, state=None, normalize=True, normalization_data=None, occlusion=False):
+        if state == None:
+            state_temp = self.state
+            state = self.state + self.true_env_corner
+            state_arr = np.array([[state[0], state[1], self.orientation]])
+        else:
+            state_temp = state
+            state = state + self.true_env_corner
+            state_arr = np.array([state])
+
+        path = os.getcwd() + '/images/' 
+        #os.mkdir(path)
+        check_path(path)
+
+        img_path, traversible, dx_m = generate_observation(state_arr, path)
+        image = cv2.imread(img_path, cv2.IMREAD_COLOR)
+        #image = np.random.randint(255, size=(64, 64, 3))
+        
+        if occlusion:
+            out = self.noise_image_occlusion(image, state_temp)
+        else:
+            out = self.noise_image_plane(image, state_temp)
+
+        out = out[:, :, ::-1]  ## CV2 works in BGR space instead of RGB!! So dumb! --- now image is in RGB
+        out = np.ascontiguousarray(out)
+
+        if normalize:
+            rmean, gmean, bmean, rstd, gstd, bstd = normalization_data
+            img_rslice = (out[:, :, 0] - rmean)/rstd
+            img_gslice = (out[:, :, 1] - gmean)/gstd
+            img_bslice = (out[:, :, 2] - bmean)/bstd
+
+            out = np.stack([img_rslice, img_gslice, img_bslice], axis=-1)
+
+            #out = (out - out.mean())/out.std()  # "Normalization" -- TODO
+
+        os.remove(img_path)
+        os.rmdir(path)
+
+        return out, img_path, traversible, dx_m
 
     def noise_image(self, image, state, noise_amount=sep.noise_amount):
         salt = 255
@@ -264,173 +447,6 @@ class StanfordEnvironment(AbstractEnvironment):
          #cv2.imwrite("out_debug2.png", out)
 
          return out
-    
-    def get_observation(self, state=None, normalize=True, normalization_data=None, occlusion=False):
-        if state == None:
-            state_temp = self.state
-            state = self.state + self.true_env_corner
-            state_arr = np.array([[state[0], state[1], self.orientation]])
-        else:
-            state_temp = state
-            state = state + self.true_env_corner
-            state_arr = np.array([state])
-
-        path = os.getcwd() + '/images/' 
-        #os.mkdir(path)
-        check_path(path)
-
-        img_path, traversible, dx_m = generate_observation(state_arr, path)
-        image = cv2.imread(img_path, cv2.IMREAD_COLOR)
-        
-        if occlusion:
-            out = self.noise_image_occlusion(image, state_temp)
-        else:
-            out = self.noise_image_plane(image, state_temp)
-
-        out = out[:, :, ::-1]  ## CV2 works in BGR space instead of RGB!! So dumb! --- now image is in RGB
-        out = np.ascontiguousarray(out)
-
-        if normalize:
-            rmean, gmean, bmean, rstd, gstd, bstd = normalization_data
-            img_rslice = (out[:, :, 0] - rmean)/rstd
-            img_gslice = (out[:, :, 1] - gmean)/gstd
-            img_bslice = (out[:, :, 2] - bmean)/bstd
-
-            out = np.stack([img_rslice, img_gslice, img_bslice], axis=-1)
-
-            #out = (out - out.mean())/out.std()  # "Normalization" -- TODO
-
-        os.remove(img_path)
-        os.rmdir(path)
-
-        return out, img_path, traversible, dx_m
-
-    def point_to_map(self, pos_2, cast_to_int=True):
-        """
-        Convert pos_2 in real world coordinates
-        to a point on the map.
-        """
-        map_pos_2 = pos_2 / self.dx - self.map_origin
-        if cast_to_int:
-            map_pos_2 = map_pos_2.astype(np.int32)
-        return map_pos_2
-
-    def detect_collision(self, state):
-        # Returns true if you collided
-        # Map value 0 means there's an obstacle there
-
-        # Don't hit rectangle boundaries
-        if state[0] < self.xrange[0] or state[0] > self.xrange[1]:
-            return True 
-        if state[1] < self.yrange[0] or state[1] > self.yrange[1]:
-            return True
-
-        # Check if state y-value is the same as trap/goal but it's not in the trap or goal - that's a wall
-        # if self.in_trap([self.trap_x[0][0], state[1]]) and \
-        #     not self.in_trap(state) and not self.in_goal(state):
-        #     return True
-        if (state[1] >= self.trap_y[0] and state[1] <= self.trap_y[1]) and \
-            not self.in_trap(state) and not self.in_goal(state):
-            return True
-
-        map_state = self.point_to_map(np.array(state[:sep.dim_state] + self.true_env_corner))
-        map_value = self.traversible[map_state[1], map_state[0]]
-        collided = (map_value == 0)
-        return collided
-    
-    def in_trap(self, state):
-        # Returns true if in trap
-        first_trap = self.trap_x[0]
-        first_trap_x = (state[0] >= first_trap[0] and state[0] <= first_trap[1])
-        second_trap = self.trap_x[1]
-        second_trap_x = (state[0] >= second_trap[0] and state[0] <= second_trap[1])
-        trap_x = first_trap_x or second_trap_x
-
-        trap = trap_x and (state[1] >= self.trap_y[0] and state[1] <= self.trap_y[1]) 
-
-        # Traps that may appear during test time -- optional
-        if self.test_trap:
-            first_test_trap = self.test_trap_x[0]
-            first_test_trap_x = (state[0] >= first_test_trap[0] and state[0] <= first_test_trap[1])
-            second_test_trap = self.test_trap_x[1]
-            second_test_trap_x = (state[0] >= second_test_trap[0] and state[0] <= second_test_trap[1])
-            #test_trap_x = first_test_trap_x or second_test_trap_x
-
-            first_test_trap = self.test_trap_y[0]
-            first_test_trap_y = (state[1] >= first_test_trap[0] and state[1] <= first_test_trap[1])
-            second_test_trap = self.test_trap_y[1]
-            second_test_trap_y = (state[1] >= second_test_trap[0] and state[1] <= second_test_trap[1])
-            
-            test_trap = (first_test_trap_x and first_test_trap_y) or \
-                        (second_test_trap_x and second_test_trap_y)
-
-            # test_trap = test_trap_x and (state[1] >= self.test_trap_y[0] and state[1] <= self.test_trap_y[1])
-            
-            return (trap or test_trap) and not self.in_goal(state)
-        
-        return trap and not self.in_goal(state)
-    
-    def in_goal(self, state):
-        # Returns true if in goal
-        goal = (state[0] >= self.target_x[0] and state[0] <= self.target_x[1]) and \
-                (state[1] >= self.target_y[0] and state[1] <= self.target_y[1])
-        return goal
-
-    # def step(self, action):
-    #     obs = self.observation_space.sample()
-    #     # reward = self._random.uniform(0, 1)
-    #     reward = np.random.uniform(0, 1)
-    #     self._step += 1
-    #     #done = self._step >= 1000
-    #     done = self._step >= 10
-    #     #done = self._step >= 200
-    #     info = {}
-    #     return obs, reward, done, info
-
-    def step(self, action, action_is_vector=False):
-        episode_length = 50
-
-        #self.done = False
-        curr_state = self.state
-
-        # Get the observation at the current state to provide PlaNet the expected output
-        normalization_data = self.preprocess_data()
-        obs_nav, _, _, _ = self.get_observation(normalization_data=normalization_data)
-        obs = OrderedDict()        
-        obs['image'] = obs_nav
-
-        #obs = self.observation_space.sample()
-        
-        if action_is_vector:
-            new_theta = np.arctan2(action[1], action[0])
-            if new_theta < 0:  # Arctan stuff
-                new_theta += 2*np.pi
-            next_state = curr_state + action
-        else:
-            new_theta = action[0] * np.pi + np.pi
-            vector = np.array([np.cos(new_theta), np.sin(new_theta)]) * sep.velocity  # Go in the direction the new theta is
-            next_state = curr_state + vector
-    
-        cond_hit = self.detect_collision(next_state)
-
-        if self.in_goal(next_state):
-            self.state = next_state
-            self.orientation = new_theta
-            #self.done = True
-        elif cond_hit == False:
-            self.state = next_state
-            self.orientation = new_theta
-        #reward = sep.epi_reward * self.done
-        reward = sep.epi_reward * self.in_goal(next_state)
-
-        cond_false = self.in_trap(next_state)
-        reward -= sep.epi_reward * cond_false
-
-        self.done = self._step >= episode_length
-
-        info = {}
-        self._step += 1
-        return obs, reward, self.done, info
 
     def preprocess_data(self):
         # For normalizing the images - per channel mean and std
