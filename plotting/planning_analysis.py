Fixing bug in statistics calculation

plotting/planning_analysis.py
@@ -5,7 +5,7 @@ import pickle
 WARNING: This code assumes the action repeat is 2!
 '''
 
-planning_times = pickle.load(open("planning_times.p", "rb"))
+planning_times = pickle.load(open("planning_times_2.p", "rb"))
 # print(planning_times)
 
 planning_times_per_episode = []
@@ -17,29 +17,35 @@ while i < len(planning_times) and planning_times[i][0] != "batch_env":
     i += 1
 planning_times = planning_times[i:]
 dones = np.array([time[1] for time in planning_times])
-print("DONES", np.where(dones == True))
-print(planning_times[-50:])
+print("DONES", np.where(dones == True), len(np.where(dones == True)[0]))
+# print(planning_times[-50:])
 
 i = 0  # Planning times counter, starts at an instance of "batch_env"
 j = 1  # Batch env counter 
 prev_time = planning_times[i][1] # Starting time
 episode_time = 0 
 num_steps = 0
+num_steps_counter = 0
 while i < len(planning_times):
     # Advance batch env counter to the next instance of "batch_env"
     while j < len(planning_times) and planning_times[j][0] != "batch_env":
         j += 1
+    # print("i, j", i, j)
     encoder = planning_times[i + 1][1]
     stanford_client = 0
     stanford_client_reached_goal = False
     stanford_client_done = False
-    for k in range(i + 1, j):
+    for k in range(i + 2, j):
         if planning_times[k][0] == "stanford_client":
             stanford_client += planning_times[k][1]
+            num_steps_counter += 1
+            # print("STANFORD CLIENT", stanford_client)
         elif planning_times[k][0] == "stanford_client_reached_goal":
             stanford_client_reached_goal += planning_times[k][1]
+            # print("STANFORD CLIENT REACHED GOAL", stanford_client_reached_goal)
         elif planning_times[k][0] == "stanford_client_done":
             stanford_client_done += planning_times[k][1]
+            # print("STANFORD CLIENT DONE", stanford_client_done)
     
     to_subtract = 2*encoder  # Time spent running the encoder, once in batch_env and once for real
     to_subtract += stanford_client  # Time spent stepping in environment
@@ -49,7 +55,7 @@ while i < len(planning_times):
         if batch_env != prev_time:
             step_time = batch_env - prev_time
             step_time -= to_subtract
-            print("STEP TIME", step_time)
+            # print("STEP TIME", step_time)
             episode_time += step_time 
     
     # if j - i == 5: 
@@ -64,9 +70,10 @@ while i < len(planning_times):
     if stanford_client_reached_goal or j >= len(planning_times): # End of the episode
         episode_time /= num_steps  # TODO (sdeglurkar): how to handle action repeat
         planning_times_per_episode.append(episode_time)
-        print("Average planning time for the episode:", episode_time)
+        print("Average planning time for the episode:", episode_time, "NUM STEPS", num_steps_counter)
         episode_time = 0  # Reset the episode time and num step counts
         num_steps = 0
+        num_steps_counter = 0
 
     prev_time = batch_env
     i = j

plotting/stanford_viz.py
@@ -88,12 +88,12 @@ def plot_maze(episode, figure_name_folder, figure_name_name, test_traps=None):
 def find_steps_taken(episode):
     step_goal_reached = np.where(episode['reached_goal'] == True)[0]
     if len(step_goal_reached) == 0:
-        return step_goal_reached, [len(episode['reached_goal'])] # Length of episode
+        return step_goal_reached, [len(episode['reached_goal']) - 1] # Length of episode
     
     steps_taken = [step_goal_reached[0]]
     for i in range(len(step_goal_reached) - 1):
         # Successive times goal is reached
-        steps_taken.append(step_goal_reached[i + 1] - step_goal_reached[i])
+        steps_taken.append(step_goal_reached[i + 1] - step_goal_reached[i] - 1)
 
     print("FIND STEPS TAKEN", step_goal_reached, steps_taken)
 
@@ -125,12 +125,14 @@ def dump_pickle(episode, figure_name_folder):
         episode_dict = pickle.load(open(BASE_FOLDER + figure_name_folder + "/" + "episode_info.p", "rb"))
     except (OSError, IOError) as e:
         episode_dict = {'reward': rewards, 
-                        'steps_taken': steps_taken}
+                        'steps_taken': steps_taken,
+                        'step_goal_reached': [step_goal_reached]}
         pickle.dump(episode_dict, open(BASE_FOLDER + figure_name_folder + "/" + "episode_info.p", "wb"))
         return
     
     episode_dict['reward'].extend(rewards)
     episode_dict['steps_taken'].extend(steps_taken)
+    episode_dict['step_goal_reached'].append(step_goal_reached)
     pickle.dump(episode_dict, open(BASE_FOLDER + figure_name_folder + "/" + "episode_info.p", "wb"))
 
 def visualize_learning(episode, figure_name_folder):

plotting/statistics.py
@@ -1,19 +1,23 @@
 import numpy as np
 import pickle
 
-BASE_FOLDER = '092422-1-testing/00001/test_episodes/' 
+BASE_FOLDER = '092422-1-testing-2/00001/test_episodes/' 
 MAX_STEPS = 200
 NUM_DESIRED_EPISODES = 500
 
 episode_dict = pickle.load(open(BASE_FOLDER + "episode_info.p", "rb"))
 rewards = episode_dict['reward']
 steps_taken = episode_dict['steps_taken']
+step_goal_reached = episode_dict['step_goal_reached']
 
 # Don't count the first episode - it's random
 print("Number of Testing Episodes:", len(np.array(rewards[1:])))
 rewards = np.array(rewards[1:])[:NUM_DESIRED_EPISODES]
 steps_taken = np.array(steps_taken[1:])[:NUM_DESIRED_EPISODES]
-print(steps_taken)
+step_goal_reached = step_goal_reached[1:][:NUM_DESIRED_EPISODES]
+print("STEPS TAKEN", steps_taken)
+print("STEP GOAL REACHED", step_goal_reached)
+
 print("Success Rate:", np.sum([elem < MAX_STEPS - 1 for elem in steps_taken])/len(steps_taken))
 successful_episodes = np.argwhere(steps_taken < MAX_STEPS - 1).flatten()
 rewards = rewards[successful_episodes]