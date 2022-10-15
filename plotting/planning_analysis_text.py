dcf0a9bd5bd962921841ce5d4b205c5f745d8dd3
sdeglurkar <sampada@visual-pomdp-test-1.c.uncertainty-aware-learning.internal>
2022-10-15 19:27:02 +0000

    Accounting for thrown out data and action repeat

--- plotting/planning_analysis_text.py
+++ plotting/planning_analysis_text.py
@@ -7,6 +7,7 @@ WARNING: This code assumes the action repeat is 2!
 MAX_STEPS = 200
 STEP_TIME_THRES = 2.0
 ENCODER_THRES = 0.2
+NUM_DESIRED_EPISODES = 500
 
 planning_time_file = "planning_times.txt"  
 
@@ -21,28 +22,28 @@ with open(planning_time_file, 'r') as f:
     # Now line = "batch_env\n"
     i = 0  # Planning times counter, starts at an instance of "batch_env"
     j = 1  # Batch env counter 
-    episode_time = 0 
-    num_steps = 0
-    num_steps_counter = 0 
-    done_counter = 0 
+    episode_time = 0  # Total time to complete 1 episode
+    num_steps_thrown_out = 0  # Counter for the data that is being thrown out
+    num_steps_counter = 0  # Length of episode in steps
+    done_counter = 0   # Counts up to MAX_STEPS
     line = f.readline() # Line after "batch_env\n"
     prev_time = float(line)
     batch_env = float(line)
    
-    encoder = 0
-    stanford_client = 0
-    stanford_client_reached_goal = False
-    stanford_client_done = False
+    encoder = 0  # Time to run encoder for 1 batch_env step
+    stanford_client = 0  # Time in stanford environment for 1 batch_env step
+    num_steps_in_env = 0  # Will always be either 1 or 2 if action repeat -- per batch_env step
+    stanford_client_reached_goal = False  # Per batch_env step
+    stanford_client_done = False  # Per batch_env step
     line = f.readline() # This will begin 2 lines after "batch_env\n"
     while line != '': 
         if line == "batch_env\n":
             batch_env = float(f.readline())
         elif line == "encoder\n":
             encoder = float(f.readline())
-            # if encoder > ENCODER_THRES:
-            #     print("\n\n\n ENCODER", num_steps_counter, "\n\n\n")
         elif line == "stanford_client\n":
             stanford_client += float(f.readline())
+            num_steps_in_env += 1
             num_steps_counter += 1
             done_counter += 1
         elif line == "stanford_client_reached_goal\n":
@@ -59,49 +60,44 @@ with open(planning_time_file, 'r') as f:
             to_subtract = 2*encoder  # Time spent running the encoder, once in batch_env and once for real
             to_subtract += stanford_client  # Time spent stepping in environment
             step_time = batch_env - prev_time
-            # print(step_time, encoder, stanford_client)
             step_time -= to_subtract
-            # if stanford_client < 0.1 or stanford_client > 0.4 and step_time < STEP_TIME_THRES:
-            #     print(stanford_client, step_time)
-            # print(step_time)
 
             # Hack -- after every 200 steps the code freezes up and step_time is not accurate!
             # Throw out that data
             # Another time the data is not accurate is when the encoder timing is for some reason too large
             if step_time < STEP_TIME_THRES and encoder < ENCODER_THRES: 
                 episode_time += step_time 
-
-            num_steps += 1
+            else:
+                num_steps_thrown_out += num_steps_in_env
 
             if stanford_client_reached_goal: # End of the episode
-                episode_time /= num_steps  # TODO (sdeglurkar): how to handle action repeat
+                episode_time /= (num_steps_counter - num_steps_thrown_out)
                 planning_times_per_episode.append(episode_time)
                 print("Average planning time for the episode:", episode_time, "NUM STEPS", num_steps_counter) 
                 episode_time = 0  # Reset the episode time and num step counts
-                num_steps = 0
                 num_steps_counter = 0 
+                num_steps_thrown_out = 0
             
             if done_counter >= MAX_STEPS - 1: # Done
                 if num_steps_counter >= MAX_STEPS - 1: # A full episode
-                    episode_time /= num_steps  # TODO (sdeglurkar): how to handle action repeat
+                    episode_time /= (num_steps_counter - num_steps_thrown_out)  
                     planning_times_per_episode.append(episode_time)
                     print("Average planning time for the episode DONE:", episode_time, "NUM STEPS", num_steps_counter)
                 episode_time = 0  # Reset the episode time and num step counts
-                num_steps = 0
                 num_steps_counter = 0 
                 done_counter = 0 
+                num_steps_thrown_out = 0
 
             prev_time = batch_env
             encoder = 0
             stanford_client = 0
+            num_steps_in_env = 0
             stanford_client_reached_goal = False
             stanford_client_done = False
-
-
 
 
         line = f.readline()
 
         
-planning_times_per_episode = np.array(planning_times_per_episode)
+planning_times_per_episode = np.array(planning_times_per_episode[:NUM_DESIRED_EPISODES])
 print("\nAverage planning time over all episodes: ", np.mean(planning_times_per_episode))        
